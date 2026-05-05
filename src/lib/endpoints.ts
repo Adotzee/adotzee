3d227bce99aa96e2dc2b68ce3f7be69917b783e3
Adotzee <adotzeein@gmail.com>
2026-05-05 23:41:35 +0530

    cooked

--- src/lib/endpoints.ts
+++ src/lib/endpoints.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.adotzee.in/api";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://adotzeebackend.onrender.com/api";
 
 export const ENDPOINTS = {
     COURSES: {
